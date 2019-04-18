85d83ce3b0366b945f1aebabddbe0293840a2fa2
Saif Anwar <anwarsaif99@hotmail.com>
2019-04-18 15:10:43 +0100

    knn with every possibilty of features
    for loop at the end runs knn for every possibility

--- Coursework_2/knn.py
+++ Coursework_2/knn.py
@@ -23,16 +23,21 @@ colours[train_labels == 2] = class_2_colour
 colours[train_labels == 3] = class_3_colour
 
 
-#will use feature pair 10 and 13 for now but not yet finalised
-
-#####calculating distance/similarity between desired wine and every other wine
-def euclideanDistance(train_set,test_set, wineNo):
-    features = [2,8,12]
+###########################################################
+def feature_selection(train_set, train_labels, f, x, y):
+    if f == 3:
+        selected_features =[9,10,13]
+    else:
+        selected_features = [x,y]
+    return selected_features
+###########all functions required for knn###########################
+def euclideanDistance(train_set,test_set, wineNo, f, x, y):
+    selected_features = np.array(feature_selection(train_set, train_labels, f, x, y))-1
     wine1=test_set[wineNo-1:wineNo].astype(np.float)
     allDistances = []
     for y in range(0,125):
         distance = 0
-        for x in features:
+        for x in selected_features:
             wine2 = train_set[y,x].astype(np.float)
             distance += np.power(wine1[:,x]-wine2, 2)
         allDistances = np.append(allDistances, distance )
@@ -40,18 +45,18 @@ def euclideanDistance(train_set,test_set, wineNo):
 
 
 #gets k nearest neighbours
-def nearestNeighbours(train_set, test_set, wineNo, k):
+def nearestNeighbours(train_set, test_set, wineNo, k, f, x, y):
     neighbours = []
-    distances = euclideanDistance(train_set, test_set, wineNo)
+    distances = euclideanDistance(train_set, test_set, wineNo, f, x, y)
     for x in range(0,k):
         neighbours = np.append(neighbours, np.amin(distances))
         distances = np.delete(distances, (np.argwhere(distances == np.amin(distances))))
     return neighbours
 
 #assigns class to wine in question
-def classify(train_set, train_labels, test_set, wineNo, k):
-    distances = euclideanDistance(train_set, test_set, wineNo)
-    neighbours = nearestNeighbours(train_set, test_set, wineNo, k)
+def classify(train_set, train_labels, test_set, wineNo, k, f, x, y):
+    distances = euclideanDistance(train_set, test_set, wineNo, f, x, y)
+    neighbours = nearestNeighbours(train_set, test_set, wineNo, k, f, x, y)
     classes = []
     for i in range(k):
         # if there are multiple neighbours at same distance away then pick first one
@@ -71,18 +76,23 @@ def calculate_accuracy(gt_labels, pred_labels):
     for i in range(0,total):
         if (pred_labels.item(i) != gt_labels.item(i)):
             totalWrong += 1
-            accuracy = str(((total-totalWrong)/total)*100)
-    print(accuracy + '%')
+    accuracy = str(((total-totalWrong)/total)*100)
+#    print(accuracy + '%')
     return accuracy
 
-
-def knn(train_set, train_labels, test_set, k):
+######calculates predictions for all wines of test_set
+def knn(train_set, train_labels, test_set, k, x, y):
+    f=2
     predictions = []
     for i in range(1,54):
-        predictions = np.append(predictions, classify(train_set, train_labels, test_set, i, k))
-    accuracy = calculate_accuracy(test_labels, predictions)
+        predictions = np.append(predictions, classify(train_set, train_labels, test_set, i, k, f, x, y))
+        accuracy = calculate_accuracy(test_labels, predictions)
     return accuracy, predictions
 
-accuracy, predictions = knn(train_set, train_labels, test_set, 1)
-print(predictions)
-np.savetxt('results.csv', predictions, delimiter=',', fmt='%d')
+for x in range(1,14):
+    for y in range(1,14):
+        accuracy, predictions = knn(train_set, train_labels, test_set, 1, x, y)
+        print([x,y])
+        print(accuracy)
+
+#np.savetxt('results.csv', predictions, delimiter=',', fmt='%d')
