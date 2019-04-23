820893421bf06e7868a395d2dfa0450a7c56fa1a
Saif Anwar <anwarsaif99@hotmail.com>
2019-04-23 14:52:30 +0100

    changed mode
    changed way we select mode. recursively calls function for k-1 if there are multiple modes for k neighbours

--- Coursework_2/wine_classifier.py
+++ Coursework_2/wine_classifier.py
@@ -13,12 +13,13 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from utilities import load_data, print_features, print_predictions
+from statistics import mode, StatisticsError
 # you may use these colours to produce the scatter plots
 CLASS_1_C = r'#3366ff'
 CLASS_2_C = r'#cc3300'
 CLASS_3_C = r'#ffc34d'
 
-MODES = ['feature_sel', 'knn', 'alt', 'knn_3d', 'knn_pca', 'feature_plots']
+TASKS = ['feature_sel', 'knn', 'alt', 'knn_3d', 'knn_pca', 'feature_plots']
 
 train_labels, train_set, test_labels, test_set = load_data()
 ################################plot features##########################################
@@ -39,12 +40,12 @@ def subplots(dataset, n, **kwargs):
     for x in range(0,n):
        for y in range(0,n):
            ax[x,y].scatter(dataset[:,x], dataset[:, y], c=colours)
-###########################################################
+#######################select features between 1-13####################################
 def feature_selection(train_set, train_labels, f, **kwargs):
     if f == 3:
         selected_features =[1,7,2]
     else:
-        selected_features = [7,10]
+        selected_features = [1,7]
     return selected_features
 ###########all functions required for knn###########################
 def euclideanDistance(train_set,test_set, wineNo, f):
@@ -78,13 +79,14 @@ def classify(train_set, train_labels, test_set, wineNo, k, f):
         # if there are multiple neighbours at same distance away then pick first one
         iclass = train_labels[np.argwhere(distances == (neighbours.item(i))).item(0)]
         classes = np.append(classes, iclass)
-    (values,counts) = np.unique(classes,return_counts=True)
-    ind=np.argmax(counts)
-    wineClass = values[ind]
-
+    try:
+        wineClass = mode(classes)
+    except StatisticsError:
+        wineClass = classify(train_set, train_labels, test_set, wineNo, k-1, f)
     return wineClass
 
-    ###########ACCURACY#######################
+
+###########ACCURACY#######################
 
 def calculate_accuracy(gt_labels, pred_labels):
     total = pred_labels.size
@@ -92,7 +94,7 @@ def calculate_accuracy(gt_labels, pred_labels):
     for i in range(0,total):
         if (pred_labels.item(i) != gt_labels.item(i)):
             totalWrong += 1
-            accuracy = str(((total-totalWrong)/total)*100)
+    accuracy = str(((total-totalWrong)/total)*100)
     print(accuracy + '%')
     return accuracy
 
@@ -129,7 +131,7 @@ def knn_pca(train_set, train_labels, test_set, k, n_components=2, **kwargs):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', nargs=1, type=str, help='Running mode. Must be one of the following modes: {}'.format(MODES))
+    parser.add_argument('task', nargs=1, type=str, help='Running task. Must be one of the following tasks: {}'.format(TASKS))
     parser.add_argument('--k', nargs='?', type=int, default=1, help='Number of neighbours for knn')
     parser.add_argument('--f', nargs='?', type=int, default=1, help='Number of features to display for feature_sel')
     parser.add_argument('--train_set_path', nargs='?', type=str, default='data/wine_train.csv', help='Path to the training set csv')
@@ -138,38 +140,38 @@ def parse_args():
     parser.add_argument('--test_labels_path', nargs='?', type=str, default='data/wine_test_labels.csv', help='Path to the test labels csv')
 
     args = parser.parse_args()
-    mode = args.mode[0]
+    task = args.task[0]
 
 
-    return args, mode
+    return args, task
 
 
 if __name__ == '__main__':
-    args, mode = parse_args() # get argument from the command line
+    args, task = parse_args() # get argument from the command line
 
     # load the data
     train_set, train_labels, test_set, test_labels = load_data(train_set_path=args.train_set_path,
                                                                        train_labels_path=args.train_labels_path,
                                                                        test_set_path=args.test_set_path,
                                                                        test_labels_path=args.test_labels_path)
-    if mode == 'feature_sel':
+    if task == 'feature_sel':
         selected_features = feature_selection(train_set, train_labels, args.f)
         print_features(selected_features)
-    elif mode == 'knn':
+    elif task == 'knn':
         predictions = knn(train_set, train_labels, test_set, args.k)
         print_predictions(predictions)
-    elif mode == 'alt':
+    elif task == 'alt':
         predictions = alternative_classifier(train_set, train_labels, test_set)
         print_predictions(predictions)
-    elif mode == 'knn_3d':
+    elif task == 'knn_3d':
         predictions = knn_three_features(train_set, train_labels, test_set, args.k)
         print_predictions(predictions)
-    elif mode == 'knn_pca':
+    elif task == 'knn_pca':
         prediction = knn_pca(train_set, train_labels, test_set, args.k)
         print_predictions(prediction)
         #plots
-    elif mode == 'feature_plots':
+    elif task == 'feature_plots':
         subplots(train_set1, n_features)
         plt.show()
     else:
-        raise Exception('Unrecognised mode: {}. Possible modes are: {}'.format(mode, modeS))
+        raise Exception('Unrecognised task: {}. Possible tasks are: {}'.format(task, TASKS))
